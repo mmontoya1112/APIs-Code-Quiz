2decef8cb6a4e6adccce6ea55e6fe85e06d36bc6
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-05-21 15:06:24 -0700

    added js variables

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var startQuiz = document.getElementById("start-button");
+var viewHigh = document.getElementById("view-high-scores");
+var quizScores = document.getElementById("scores");
+var quizQuestion = document.getElementById("question");
+var quizChoices = document.getElementById("choices");
+var quizResults = document.getElementById("results");
+var quizButton = document.getElementById("buttons");
+var startButton = document.getElementById("start-button");
+
