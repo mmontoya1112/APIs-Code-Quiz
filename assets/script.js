34ea7b2e2e0357d8d7b13f4176d9583c3db7dce0
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-05-21 17:56:49 -0700

    deleted comments

--- assets/script.js
+++ assets/script.js
@@ -8,25 +8,6 @@ var quizButton = document.getElementById("buttons");
 var startButton = document.getElementById("start-button");
 
 
-
-
-/*startQuiz.addEventListener("click", function(){
-    var timeleft = 15;
-
-    var downloadTimer = setInterval(function function1(){
-    document.getElementById("countdown").innerHTML = timeleft + 
-    "&nbsp"+"seconds remaining";
-
-    timeleft -= 1;
-    if(timeleft <= 0){
-        clearInterval(downloadTimer);
-        document.getElementById("countdown").innerHTML = "Time is up!"
-    }
-    }, 1000);
-
-    console.log(countdown);
-});*/
-
 startQuiz.addEventListener("click", function(){
     var timeleft = 15;
 
