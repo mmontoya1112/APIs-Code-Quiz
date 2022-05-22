b0b3db06b55c6f88d9274a80100d38f3bf93d315
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-05-21 17:45:20 -0700

    added a timer button

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,39 @@ var quizResults = document.getElementById("results");
 var quizButton = document.getElementById("buttons");
 var startButton = document.getElementById("start-button");
 
+
+
+
+/*startQuiz.addEventListener("click", function(){
+    var timeleft = 15;
+
+    var downloadTimer = setInterval(function function1(){
+    document.getElementById("countdown").innerHTML = timeleft + 
+    "&nbsp"+"seconds remaining";
+
+    timeleft -= 1;
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "Time is up!"
+    }
+    }, 1000);
+
+    console.log(countdown);
+});*/
+
+startQuiz.addEventListener("click", function(){
+    var timeleft = 15;
+
+    var downloadTimer = setInterval(function function1(){
+    document.getElementById("countdown").innerHTML = timeleft + 
+    "&nbsp"+"seconds remaining";
+
+    timeleft -= 1;
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "Time is up!"
+    }
+    }, 1000);
+
+    console.log(countdown);
+});
