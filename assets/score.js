17b8d96aa44204c35182c8ae2da9241079ea76b2
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-05-24 10:15:42 -0700

    added highscores html and js

--- assets/score.js
+++ assets/score.js
@@ -1,7 +1,8 @@
 const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = document.querySelector('#mostRecentScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
+/*might need a # */
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 const MAX_HIGH_SCORES = 5
