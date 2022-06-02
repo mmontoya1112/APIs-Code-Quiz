9dfc6e2ccb7b838ef7987c6654658b7bd096b800
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-06-01 18:19:34 -0700

    fixed more paths and moved htmls out of the assets folder

--- assets/score.js
+++ assets/score.js
@@ -26,5 +26,5 @@ username.addEventListener('keyup', () => {
     highScores.splice(5)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/assets/highScores.html')
+    window.location.assign('./highScores.html')
 }
